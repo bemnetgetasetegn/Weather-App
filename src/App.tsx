3672f035947eb1b271d329b3e70f9f8df9eceb87
bemnetgetasetegn <bigbemnet@gmail.com>
2025-05-03 22:46:29 +0300

    navbar style improved

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Weather from "./assets/Components/Weather";
 
 const App = () => {
   return (
-    <div className="flex justify-center items-center flex-col bg-[url('./assets/Pictures/dat.png')] bg-cover bg-center h-screen">
+    <div className="flex justify-center items-center flex-col bg-[linear-gradient(rgba(0,0,0,0.2),rgba(0,0,0,0.2)),url('./assets/Pictures/dat.png')] bg-cover bg-center h-screen">
       <div className="w-screen p-10">
         <NavBar />
         <Weather />
--- src/assets/Components/NavBar.tsx
+++ src/assets/Components/NavBar.tsx
@@ -4,13 +4,12 @@ import logo from "../Pictures/WeatherLogo.png";
 const NavBar = () => {
   const ref = useRef<HTMLInputElement>(null);
   return (
-    <div className="flex backdrop-blur-[2px] w-full border border-white my-10 p-2 t justify-around items-center rounded-2xl">
+    <div className="flex backdrop-blur-[2px] w-full border-[1px] border-gray-400 my-10 py-3 t justify-around items-center rounded-2xl">
       <div className="flex items-center">
       <img src={logo} alt="" className="size-10 rounded-2xl" /> <span className="px-3 text-white font-bold text-2xl">Weather App</span>
       </div>
       <nav className="">
         <form
-        className="w-48"
           action=""
           onSubmit={(e) => {
             e.preventDefault();
@@ -18,7 +17,7 @@ const NavBar = () => {
             }
           }}
         >
-          <input className="text-white border-2 p-2 rounded-2xl" ref={ref} type="text" placeholder="Search for a city..." />
+          <input className="text-white border-[1px] placeholder-white border-gray-400 md:w-2xl p-2 rounded-2xl" ref={ref} type="text" placeholder=" Search for a city..." />
         </form>
       </nav>
     </div>
