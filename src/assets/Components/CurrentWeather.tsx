c85812cb531c92d7c30bdebb320fa3773cd92fc2
bemnetgetasetegn <bigbemnet@gmail.com>
2025-05-06 20:21:14 +0300

    finished weatherMetrics

--- src/assets/Components/CurrentWeather.tsx
+++ src/assets/Components/CurrentWeather.tsx
@@ -10,9 +10,9 @@ interface Props {
 const CurrentWeather = ({ current, location }: Props) => {
   return (
     <div className="border transform transition-transform hover:scale-102 border-white/20 rounded-2xl">
-      <div className="flex justify-between">
+      <div className="relative flex justify-between">
         <div className="p-4 space-y-3 flex flex-col">
-          <span className="text-7xl ">{current?.temp_f}&deg; </span>
+          <span className="text-7xl ">{current?.temp_f}&deg;F </span>
           <span className="text-4xl whitespace-nowrap">
             {current?.condition.text}
           </span>
@@ -23,7 +23,7 @@ const CurrentWeather = ({ current, location }: Props) => {
             <span>{TwelveHrFormat(location?.localtime.substring(11))}</span>
           </span>
         </div>
-        <div className="">
+        <div className="lg:absolute top-10 right-20">
           <img width={"100px"} src={current?.condition.icon} alt="" />
         </div>
       </div>
--- src/assets/Components/WeatherMetrics.tsx
+++ src/assets/Components/WeatherMetrics.tsx
@@ -1,8 +1,10 @@
 import { current } from "../Entities/current";
 import windDrection from "../Pictures/WindDirections.png";
 import HeatIndex from "../Pictures/HeatIndex.png";
+import uv from "../Pictures/UV.png";
 import { FaCloud } from "react-icons/fa";
 import { WiHumidity } from "react-icons/wi";
+import { GrUpdate } from "react-icons/gr";
 
 interface Props {
   current?: current;
@@ -20,6 +22,7 @@ const WeatherMetrics = ({ current }: Props) => {
     W: "270deg",
     NW: "315deg",
   };
+
   return (
     <div className="border transform transition-transform hover:scale-102 border-white/30 rounded-2xl">
       <div className="grid  grid-cols-2 grid-rows-3  p-3 text-2xl font-bold ">
@@ -30,22 +33,23 @@ const WeatherMetrics = ({ current }: Props) => {
             className={`transform size-20`}
             style={{ transform: `rotate(${directions[current.wind_dir]})` }}
           />
-          <span className="">{current.wind_kph}Kph</span>
+          <span className="">{current.wind_kph} Kph</span>
         </div>
         <div className="flex gap-4 justify-center items-center">
-          <FaCloud size={40} /> {current.cloud}
+          <FaCloud size={40} /> {current.cloud}%
         </div>
         <div className="flex gap-4 pl-4 items-center ">
-          <WiHumidity size={50} /> {current.humidity}
+          <WiHumidity size={50} /> {current.humidity}%
         </div>
-        <div className="flex gap-4 pl-15 items-center ">
-          <img src={HeatIndex} color="white" /> {current.heatindex_f}
+        <div className="flex  pl-12 items-center ">
+          <img className="size-15" src={HeatIndex} color="white" />{" "}
+          {current.heatindex_f}&deg;F
         </div>
-        <div className="flex gap-4 pl-4 items-center ">
-          <WiHumidity size={50} /> {current.humidity}
+        <div className="flex gap-4 pl-3 items-center ">
+          <img src={uv} className="size-15" /> {current.uv}
         </div>
-        <div className="flex gap-4 pl-15 items-center ">
-          <WiHumidity size={50} /> {current.humidity}
+        <div className="flex gap-4 pl-17 items-center ">
+          <GrUpdate /> {current.last_updated.substring(11)}
         </div>
       </div>
     </div>
