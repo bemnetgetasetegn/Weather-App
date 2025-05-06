150999b8368db465d65150c3b211c2fdd5573317
bemnetgetasetegn <bigbemnet@gmail.com>
2025-05-06 21:27:42 +0300

    fixed responsiveness on navbar and currentWeather

--- src/assets/Components/CurrentWeather.tsx
+++ src/assets/Components/CurrentWeather.tsx
@@ -10,8 +10,8 @@ interface Props {
 const CurrentWeather = ({ current, location }: Props) => {
   return (
     <div className="border transform transition-transform hover:scale-102 border-white/20 rounded-2xl">
-      <div className="relative flex justify-between">
-        <div className="p-4 space-y-3 flex flex-col">
+      <div className="relative sm:flex justify-between">
+        <div className=" p-4 space-y-3 flex flex-col">
           <span className="text-7xl ">{current?.temp_f}&deg;F </span>
           <span className="text-4xl whitespace-nowrap">
             {current?.condition.text}
@@ -23,7 +23,7 @@ const CurrentWeather = ({ current, location }: Props) => {
             <span>{TwelveHrFormat(location?.localtime.substring(11))}</span>
           </span>
         </div>
-        <div className="lg:absolute top-10 right-20">
+        <div className=" lg:absolute top-10 right-20">
           <img width={"100px"} src={current?.condition.icon} alt="" />
         </div>
       </div>
--- src/assets/Components/NavBar.tsx
+++ src/assets/Components/NavBar.tsx
@@ -9,7 +9,7 @@ const NavBar = () => {
     <div className=" flex w-full border-[1px] border-white/20 my-10 py-3 transform transition-transform hover:scale-105 justify-around items-center rounded-2xl">
       <div className="flex items-center px1 md:px-10">
         <img src={logo} alt="" className="size-10 rounded-2xl" />{" "}
-        <span className="px-3  text-white font-bold whitespace-nowrap md:text-2xl">
+        <span className="md:px-3 text-white font-bold md:whitespace-nowrap text-center md:text-2xl text-[15px]">
           Weather App
         </span>
       </div>
@@ -24,7 +24,7 @@ const NavBar = () => {
           }}
         >
           <input
-            className="text-white border-[1px] focus:outline-none placeholder-white border-white/20   md:w-[432px] lg:w-[532px] p-2 rounded-2xl"
+            className="text-white border-[1px] focus:outline-none placeholder-white border-white/20 sm:w-fit  md:w-[432px] lg:w-[532px] w-full p-2 rounded-2xl"
             ref={ref}
             type="text"
             placeholder=" Search for a city..."
